6fdc85e2ed60c6544d393d0d771f2043aab1198f
aashu <sapkotasabin24@gmail.com>
2023-07-11 18:50:13 +0545

    added CRUD for comments and maintain concerns and static authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
-  # get "/articles", to:"articles#index"
-  # get "/articles/:id", to:"articles#show"
 end
